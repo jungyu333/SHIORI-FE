22b868e3c1f556683b1845f92bafc69821c177fc
jungyu333 <jungyu3826@gmail.com>
2025-07-06 12:19:47 +0900

    feat : Change BrandText.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -30,25 +30,25 @@ const MainLayout = ({ children, header, headerHeight = 0, floatButton, hideBotto
 export default MainLayout;
 
 const MainContainer = styled.main`
-  position: relative;
-  max-width: 480px;
-  min-height: calc(var(--vh, 1vh) * 100);
-  height: 100dvh;
-  margin: 0 auto;
-  background-color: ${theme.colors.white};
+    position: relative;
+    max-width: 100%;
+    min-height: calc(var(--vh, 1vh) * 100);
+    height: 100dvh;
+    margin: 0 auto;
+    background-color: ${theme.colors.white};
 `;
 
 const MainSection = styled.section<{ headerHeight: number; bottomNavHeight: number }>`
-  position: absolute;
-  top: ${(props) => props.headerHeight}px;
-  left: 0;
-  right: 0;
-  bottom: ${(props) => props.bottomNavHeight}px;
-  overflow: auto;
+    position: absolute;
+    top: ${(props) => props.headerHeight}px;
+    left: 0;
+    right: 0;
+    bottom: ${(props) => props.bottomNavHeight}px;
+    overflow: auto;
 `;
 
 const FloatButtonContainer = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: ${BOTTOM_NAV_HEIGHT + 5}px;
+    position: absolute;
+    right: 15px;
+    bottom: ${BOTTOM_NAV_HEIGHT + 5}px;
 `;
--- src/shared/ui/BrandText/BrandText.tsx
+++ src/shared/ui/BrandText/BrandText.tsx
@@ -4,16 +4,16 @@ export default function BrandText() {
   return (
     <HeaderMessage>
       <>
-        <b>사랑도 지출도</b>
+        <b>작은 마음 하나도</b>
         <br />
-        <b>코코로 하나면 충분해요</b>
+        <b>기록할 수 있는 곳</b>
         <br />
         <br />
-        함께 쓰고
+        당신의 하루를
         <br />
-        나누고
+        조용히 들어주고
         <br />
-        정리하는 공간
+        함께 정리해요
       </>
     </HeaderMessage>
   );
