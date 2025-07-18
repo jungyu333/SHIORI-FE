49022fc606ca9fea32cb1e10f7b44269863c8051
jungyu333 <jungyu3826@gmail.com>
2025-07-18 20:33:24 +0900

    feat : Add signIn Promise

--- src/features/auth/api/promise.ts
+++ src/features/auth/api/promise.ts
@@ -1,6 +1,8 @@
 import { SHIORI_BE } from '@/shared/api/axios/instance';
 import { SignUpRequestDTO } from '@/features/auth/model/request/signUp';
 import { SignUpResponseDTO } from '@/features/auth/model/response/signUp';
+import { SignInRequestDTO } from '@/features/auth/model/request/signIn';
+import { SignInResponseDTO } from '@/features/auth/model/response/signIn';
 
 export const signUp = ({ email, password, nickname }: SignUpRequestDTO) => {
   const body = {
@@ -10,4 +12,13 @@ export const signUp = ({ email, password, nickname }: SignUpRequestDTO) => {
   };
 
   return SHIORI_BE.post<SignUpRequestDTO, SignUpResponseDTO>('/user/signup', body);
+};
+
+export const signIn = ({ email, password }: SignInRequestDTO) => {
+  const body = {
+    email: email,
+    password: password,
+  };
+
+  return SHIORI_BE.post<SignInRequestDTO, SignInResponseDTO>('/user/login', body);
 };
