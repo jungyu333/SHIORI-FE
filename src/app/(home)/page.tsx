548ddd120c1dc96dcca4e18488e44c112d08f287
jungyu333 <jungyu3826@gmail.com>
2025-07-18 20:20:55 +0900

    refactor : Move CTAButton.tsx to shared/ui dir

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import BrandText from '@/shared/ui/BrandText';
 import OnboardingIcon from '@/shared/ui/OnboardingIcon';
 import * as S from './style';
-import CTAButton from '@/components/button/CTAButton';
+import CTAButton from '@/shared/ui/button/CTAButton';
 
 const HomePage = () => {
   return (
@@ -10,15 +10,17 @@ const HomePage = () => {
 
       <S.Space />
 
-      <CTAButton _hover={{ bgColor: 'secondary.500' }} color={'white'} bgColor={'primary.500'}
-                 w={{ base: '100%', md: '500px' }}
-                 h={{ base: '56px', md: '70px' }}
-                 fontSize={{ base: 'lg', md: '2xl' }}
-                 marginBottom={{ base: '30px' }}
+      <CTAButton
+        _hover={{ bgColor: 'secondary.500' }}
+        color={'white'}
+        bgColor={'primary.500'}
+        w={{ base: '100%', md: '500px' }}
+        h={{ base: '56px', md: '70px' }}
+        fontSize={{ base: 'lg', md: '2xl' }}
+        marginBottom={{ base: '30px' }}
       />
 
       <OnboardingIcon />
-
     </S.Wrapper>
   );
 };
