15223978679d31856e683534e82bb26fd0592328
jungyu333 <jungyu3826@gmail.com>
2025-08-10 19:13:41 +0900

    feat : add Wrapper styled component for page layout

---
+++ src/app/diary/week/style.ts
@@ -0,0 +1,12 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  gap: 24px;
+  padding: 0 10px;
+`;
