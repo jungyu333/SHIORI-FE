d8cdc4fd8626d9b5940cec47e43b329b8a8b2334
jungyu333 <jungyu3826@gmail.com>
2025-08-17 13:04:04 +0900

    chore : add query key for getWeekDiaryMeta

--- src/features/diary/constants/queryKeys.ts
+++ src/features/diary/constants/queryKeys.ts
@@ -1,3 +1,4 @@
 export const QUERY_KEY = {
   getDiaryContent: 'get_diary_content',
+  getWeekDiaryMeta: 'get_week_diary_meta',
 };
--- src/features/diary/model/response/getWeekDiaryMeta.ts
+++ src/features/diary/model/response/getWeekDiaryMeta.ts
@@ -1,9 +1,11 @@
+import { SummaryStatus } from '@/features/diary/model/types/week';
+
 export type GetWeekDiaryMetaResponseDTO = WeekDiaryMeta[];
 
 export type WeekDiaryMeta = {
   date: string;
   title: string;
   isArchived: boolean;
-  summaryStatus: string;
+  summaryStatus: SummaryStatus;
   updatedAt: string;
 };
